f8961bd3008e6306da1649b90e0040234aaaaeca
Not-Sarthak <notsarthakshah@gmail.com>
2024-06-20 23:10:13 +0530

    add: name in createPlan()

--- packages/hardhat/contracts/Payment.sol
+++ packages/hardhat/contracts/Payment.sol
@@ -12,6 +12,7 @@ contract Payment {
 		address token;
 		uint amount;
 		uint frequency;
+		string name;
 	}
 
 	struct Subscription {
@@ -25,7 +26,7 @@ contract Payment {
 
 	AccessControl public accessControl;
 
-	event PlanCreated(address merchant, uint planId, uint date);
+    event PlanCreated(address merchant, uint planId, uint date, string name);
 
 	event SubscriptionCreated(address subscriber, uint planId, uint date);
 
@@ -43,12 +44,13 @@ contract Payment {
 		accessControl = AccessControl(_accessControl);
 	}
 
-	function createPlan(address token, uint amount, uint frequency) external {
+	function createPlan(address token, uint amount, uint frequency, string memory name) external {
 		require(token != address(0), "address cannot be null address");
 		require(amount > 0, "amount needs to be > 0");
 		require(frequency > 0, "frequency needs to be > 0");
-		plans[nextPlanId] = Plan(msg.sender, token, amount, frequency);
-		nextPlanId++;
+		plans[nextPlanId] = Plan(msg.sender, token, amount, frequency, name);
+        emit PlanCreated(msg.sender, nextPlanId, block.timestamp, name); 
+        nextPlanId++;
 	}
 
 	function subscribe(uint planId) external {
